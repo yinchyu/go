1404ec92b6384f7755798614af642af0b557215e
yinchangyu <18898186026@163.com>
2021-11-18 16:47:57 +0800

    修改mod 模式

--- lock-free/lockfree.go
+++ lock-free/lockfree.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"runtime"
 	"runtime/pprof"
 	"sync/atomic"
+	"time"
 )
 
 type mod struct {
@@ -28,12 +30,14 @@ func putdata(queue *EsQueue) {
 		}(i)
 	}
 }
+
 type a struct {
-name int
-value int
+	name  int
+	value int
 }
-func (h *a)String() string{
-	return fmt.Sprintf("name:%v, value:%v",h.name,h.value)
+
+func (h *a) String() string {
+	return fmt.Sprintf("name:%v, value:%v", h.name, h.value)
 }
 func getdata(queue *EsQueue) {
 	for i := 0; i < 105; i++ {
@@ -45,15 +49,15 @@ func getdata(queue *EsQueue) {
 		}(i)
 	}
 }
-func test(){
-	queue:=NewQueue(math.MaxUint16)
+func test() {
+	queue := NewQueue(math.MaxUint16)
 	//putdata(queue)
 	//getdata(queue)
 	//for{
 	//}
 	queue.Put("hello world")
 	fmt.Println(queue.Get())
-	var v uint32=21
+	var v uint32 = 21
 	v--
 	v |= v >> 1
 	v |= v >> 2
@@ -62,15 +66,32 @@ func test(){
 	v |= v >> 16
 	v++
 	fmt.Println(v)
-	//fmt.Println(runtime.NumCPU())
-	//start, _ := time.Parse(time.RFC3339, "1560-01-02T15:04:05Z07:00")
-	//fmt.Println(start)
-	//end := time.Now()
-	//// 最大能记录的范围再100年以内
-	//fmt.Println(end.Sub(start).Nanoseconds())
-	//fmt.Println(math.MaxInt64)
-	ok:=atomic.CompareAndSwapUint32(&v,32,34)
-	if ok{
+	var value uint32
+	for i := 0; i > 100; i++ {
+		go func() {
+			atomic.AddUint32(&value, 1)
+		}()
+		if i%2 == 0 {
+			go func() {
+				atomic.AddUint32(&value, 2)
+			}()
+		}
+	}
+	for {
+		fmt.Println(atomic.LoadUint32(&value))
+	}
+	pproF, _ := os.Create("pprof") // 创建记录文件
+	pprof.StartCPUProfile(pproF)   // 开始cpu profile，结果写到文件f中
+	defer pprof.StopCPUProfile()
+	fmt.Println(runtime.NumCPU())
+	start, _ := time.Parse(time.RFC3339, "1560-01-02T15:04:05Z07:00")
+	fmt.Println(start)
+	end := time.Now()
+	// 最大能记录的范围再100年以内
+	fmt.Println(end.Sub(start).Nanoseconds())
+	fmt.Println(math.MaxInt64)
+	ok := atomic.CompareAndSwapUint32(&v, 32, 34)
+	if ok {
 		fmt.Println(v)
 	}
 }
@@ -84,23 +105,13 @@ func (p Person) String() string {
 	return fmt.Sprintf("%v (%v years)\n", p.Name, p.Age)
 }
 
-
 func main() {
-	//var value uint32
-	//for i:=0;i>100;i++{
-	//	go func() {
-	//		atomic.AddUint32(&value,1)
-	//	}()
-	//	if i%2==0{
-	//		go func() {
-	//			atomic.AddUint32(&value,2)
-	//		}()
-	//	}
-	//}
+
+	queue := NewQueue(100)
+	//putdata(queue)
+	//getdata(queue)
 	//for{
-	//	fmt.Println(atomic.LoadUint32(&value))
-	//	}
-	pproF, _ := os.Create("pprof") // 创建记录文件
-	pprof.StartCPUProfile(pproF)   // 开始cpu profile，结果写到文件f中
-	defer pprof.StopCPUProfile()
+	//}
+	queue.Put("hello world")
+	fmt.Println(queue.Get())
 }
