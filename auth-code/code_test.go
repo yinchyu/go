5dc53a9529020adb0d531a97f2ccfec6bf1be71a
yinchangyu <18898186026@163.com>
2021-12-26 18:11:53 +0800

    增加邮箱通知的功能

--- auth-code/code_test.go
+++ auth-code/code_test.go
@@ -6,14 +6,39 @@ import (
 	"time"
 )
 
+func init() {
+	NewClient()
+}
+
 func TestCode(t *testing.T) {
+	ttl := client.TTL("1234")
+	result, err := ttl.Result()
+	if err != nil {
+		return
+	}
+	fmt.Println(result)
+
 	fmt.Println(Gencode())
 }
 func TestGetCode(t *testing.T) {
-	NewClient()
 	for i := 0; i < 10; i++ {
 		fmt.Println(GetCode("1234"))
 		time.Sleep(time.Second * 1)
+	}
+
+}
+
+func TestSendEmail(t *testing.T) {
+	defer ClostClient()
+	code, err := GetCode("1234")
+	email := "1192867487@qq.com"
+	if err == nil {
+		if VerifyEmailFormat(email) {
+			ok := SendEmail(email, code)
+			fmt.Println(ok)
+		}
+	} else {
+		fmt.Println("发送的太过频繁")
 	}
 
 }
--- auth-code/redis.go
+++ auth-code/redis.go
@@ -1,6 +1,7 @@
 package auth_code
 
 import (
+	"fmt"
 	"github.com/go-redis/redis"
 	"log"
 	"math/rand"
@@ -16,10 +17,18 @@ func NewClient() {
 		DB:       0,
 	}
 	client = redis.NewClient(ops)
+
+}
+
+type Myerr string
+
+func (m Myerr) Error() string {
+	return "  too many  request"
 }
 
 func Gencode() string {
-	code := make([]byte, 6, 6)
+	code := make([]byte, 6)
+	rand.Seed(time.Now().Unix())
 	for i := 0; i < 6; i++ {
 		code[i] = byte(rand.Intn(10) + 48)
 	}
@@ -27,19 +36,24 @@ func Gencode() string {
 }
 
 func StoreCode(uid string, code string) {
-	if _, res := client.Set(uid, code, time.Second*5).Result(); res != nil {
+	if _, res := client.Set(uid, code, time.Minute*5).Result(); res != nil {
 		log.Println(res)
 		panic(res)
 	}
 }
 
-func GetCode(uid string) string {
+func GetCode(uid string) (string, error) {
 	getc := client.Get(uid)
 	if scode, ok := getc.Result(); ok != nil {
+		fmt.Println("redis  get key is nil", ok)
 		code := Gencode()
 		StoreCode(uid, code)
-		return code
+		return code, nil
 	} else {
-		return scode
+		return scode, Myerr("")
 	}
+}
+
+func ClostClient() {
+	client.Close()
 }
