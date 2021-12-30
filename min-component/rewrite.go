53c68445fe710dab76b6aea50181488c29274fcc
yinchangyu <18898186026@163.com>
2021-12-30 22:04:35 +0800

    增加自重写程序

---
+++ min-component/rewrite.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+func main() {
+	fmt.Printf("%s%c%s%c\n", q, 0x60, q, 0x60)
+	fmt.Printf("%c%c\n", 0x60, 0x60)
+	getwd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	create, err := os.Create(path.Join(getwd, "./s.go"))
+	if err != nil {
+		return
+	}
+	writeString, err := create.WriteString(fmt.Sprintf("%s%c%s%c\n", q, 0x60, q, 0x60))
+	if err != nil {
+		return
+	}
+	fmt.Println(writeString)
+}
+
+var q = `/* Go quine */
+package main
+
+import "fmt"
+
+func main() {
+   fmt.Printf("%s%c%s%c\n", q, 0x60, q, 0x60)
+}
+
+var q = `
