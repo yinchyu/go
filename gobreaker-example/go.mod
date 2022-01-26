7be805d2a62b0d87df2777d37ced3e54b449bfd7
yinchangyu <18898186026@163.com>
2022-01-26 21:57:55 +0800

    增加注释方便理解

---
+++ gobreaker-example/go.mod
@@ -0,0 +1,6 @@
+module breaker
+
+go 1.17
+
+
+
---
+++ gobreaker-example/http_breaker.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var cb *CircuitBreaker
+
+func init() {
+	var st Settings
+	st.Name = "HTTP GET"
+	st.ReadyToTrip = func(counts Counts) bool {
+		failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
+		return counts.Requests >= 3 && failureRatio >= 0.6
+	}
+
+	cb = NewCircuitBreaker(st)
+}
+
+// Get wraps http.Get in CircuitBreaker.
+func Get(url string) ([]byte, error) {
+	body, err := cb.Execute(func() (interface{}, error) {
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, err
+		}
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		return body, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	// 只有连接成功然后顺利读到数据才算是成功
+	return body.([]byte), nil
+}
+
+func main() {
+	body, err := Get("http://www.google.com/robots.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+}
