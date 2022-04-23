7e071ea2b9683d6e7a554b61117b83be66f144e5
yinchangyu <18898186026@163.com>
2022-04-23 23:54:09 +0800

    关于边界的测试， 如果有的时候手动声明边界，bound=[:index] ，可以帮助后边节省判断的次数

--- boundrycheck/boundry_test.go
+++ boundrycheck/boundry_test.go
@@ -9,10 +9,9 @@ func TestRange(z *testing.T) {
 	x := []int{2, 3, 5, 7, 11}
 	t := x[0]
 	var i int
-	// 先自增，然后取值，然后赋值
 	for index := 0; index < len(x); index++ {
 		x[i] = x[index]
-		fmt.Print(i)
+		//fmt.Print(i)
 		i = index
 	}
 	x[i] = t
